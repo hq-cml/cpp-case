f841a8add32c67581996d41cbebdf927826fe1b3
hq <hq_cml@163.com>
2023-04-26 08:26:53 +0800

    basic io

--- essential_cpp/chapter1/main.cpp
+++ essential_cpp/chapter1/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string user_name;
+    cout << "Please input name:" << endl;
+    cin>>user_name;
+    cout<<endl;
+    cout<<"Hello "<<user_name<<". Bye!\n";
     return 0;
 }
