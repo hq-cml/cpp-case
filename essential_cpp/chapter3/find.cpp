173aeaad4f9967a64b8fa2dc8a8ea685baea6d7c
hq <hq_cml@163.com>
2023-06-02 09:00:39 +0800

    fix

--- essential_cpp/chapter3/find.cpp
+++ essential_cpp/chapter3/find.cpp
@@ -7,6 +7,17 @@
 #include "find.h"
 using namespace std;
 
+// Note: 其他版本用到了模板，放在了头文件中！
+// 在vector中找到目标数
+int *findV1(vector<int> &vec, int target) {   // v1.0
+    for (int i=0; i<vec.size(); i++) {
+        if (vec[i] == target) {
+            return &vec[i];
+        }
+    }
+    return 0;
+}
+
 // 测试findWithIter函数的使用
 void TestFindWithIter() {
     // 基本类型
@@ -19,7 +30,7 @@ void TestFindWithIter() {
     list<int> arrList (arrInt, arrInt+8);
 
     // 普通类型使用
-    int *pi = findWithIter(arrInt, arrInt+8, 7);
+    int *pi = findV5(arrInt, arrInt+8, 7);
     if (pi!=arrInt+8) {
         cout << *pi << endl;
     } else {
@@ -27,7 +38,7 @@ void TestFindWithIter() {
     }
 
     // vector类型使用
-    vector<int>::iterator it1 = findWithIter(arrVec.begin(), arrVec.end(), 4);
+    vector<int>::iterator it1 = findV5(arrVec.begin(), arrVec.end(), 4);
     if (it1!=arrVec.end()) {
         cout << *it1 << endl;
     } else {
@@ -35,7 +46,7 @@ void TestFindWithIter() {
     }
 
     // list类型使用
-    list<int>::iterator it2 = findWithIter(arrList.begin(), arrList.end(), 10);
+    list<int>::iterator it2 = findV5(arrList.begin(), arrList.end(), 10);
     if (it2!=arrList.end()) {
         cout << *it2 << endl;
     } else {
@@ -54,14 +65,14 @@ void TestFind() {
     vector<int> arrVec (arrInt, arrInt+8);
 
     // 基本类型使用
-    int *pi = find(arrInt, arrInt+8, 3);
+    int *pi = findV4(arrInt, arrInt+8, 3);
     cout << *pi << endl;
-    float *pf = find(arrFloat, arrFloat+8, 3.0f);
+    float *pf = findV4(arrFloat, arrFloat+8, 3.0f);
     cout << *pf << endl;
     string need = "golang";
-    string *ps = find(arrStr, arrStr+4, need);
+    string *ps = findV4(arrStr, arrStr+4, need);
     cout << *ps << endl;
     // vector类型使用
-    int *pv = find(&arrVec[0], &arrVec[arrVec.size()], 4);
+    int *pv = findV4(&arrVec[0], &arrVec[arrVec.size()], 4);
     cout << *pv << endl;
 }
--- essential_cpp/chapter3/find.h
+++ essential_cpp/chapter3/find.h
@@ -5,48 +5,45 @@
 #ifndef CHAPTER3_FIND_H
 #define CHAPTER3_FIND_H
 
+#include <vector>
+using namespace std;
+
 void TestFind();
 void TestFindWithIter();
 
-// 在vector中找到目标数（用到了函数模板，暂时只能在头文件中定义）
-//int *find(vector<int> &vec, int target) {   // v1.0
-//    for (int i=0; i<vec.size(); i++) {
-//        if (vec[i] == target) {
-//            return &vec[i];
-//        }
-//    }
-//    return 0;
-//}
+// v1.0没有函数模板，只能定义在源文件中，否则会链接错误
 // 将上面的函数模板化，使得Vector不一定非要是int！
-//template <typename T>                       // v2.0
-//T* find(vector<T> &vec, T target) {
-//    for (int i=0; i<vec.size(); i++) {
-//        if (vec[i] == target) {
-//            return &vec[i];
-//        }
-//    }
-//    return 0;
-//}
+template <typename T>                       // v2.0
+T* findV2(vector<T> &vec, T target) {
+    for (int i=0; i<vec.size(); i++) {
+        if (vec[i] == target) {
+            return &vec[i];
+        }
+    }
+    return 0;
+}
+
 // 将上面的函数进一步抽象，使他不再局限于Vector类型
 // NOTE: 这里利用了C的一个特性即数组作为参数传入函数会退化指针
 //       所以直接传入一个指针就相当于传入一个数组
-//template <typename T>                        // v3.0
-//T* find(T *arr, int size, T target) {
-//    if (!arr || size < 1) {
-//        return 0;
-//    }
-//    for (int i=0; i<size; i++) {
-//        if (arr[i] == target) {
-//            return &arr[i];
-//        }
-//    }
-//    return 0;
-//}
+template <typename T>                        // v3.0
+T* findV3(T *arr, int size, T target) {
+    if (!arr || size < 1) {
+        return 0;
+    }
+    for (int i=0; i<size; i++) {
+        if (arr[i] == target) {
+            return &arr[i];
+        }
+    }
+    return 0;
+}
+
 // 更地道的优化，去除size参数，引入标兵指针
 // Note: CPP的风格，是左闭右开，即不包括标兵指针
 //       标兵仅用来进行比较，不能进行提领运算！
 template <typename T>                          // v4.0
-T* find(T *first, T *end, T target) {
+T* findV4(T *first, T *end, T target) {
     if (!first || !end) {
         return 0;
     }
@@ -67,7 +64,7 @@ T* find(T *first, T *end, T target) {
 // vector<int> vecInt;
 // vector<int>::it = vecInt.begin()
 template<typename ItType, typename T>
-ItType findWithIter(ItType first, ItType last, T target) {
+ItType findV5(ItType first, ItType last, T target) {
     for (ItType iter=first; iter!=last; iter++) {
         if (*iter == target) {
             return iter;
