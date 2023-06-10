c77c408fc570aabe3a5026d3122c1450c7170865
hq <hq_cml@163.com>
2023-06-11 07:56:39 +0800

    手动实现迭代器类Iterator Class

--- essential_cpp/chapter4/TriangularIterator.h
+++ essential_cpp/chapter4/TriangularIterator.h
@@ -6,8 +6,9 @@
 #define CHAPTER4_TRIANGULARITERATOR_H
 
 #include <iterator>
+#include <iostream>
 #include "Triangular.h"
-
+using namespace std;
 /*
  * Note: 实现自己的迭代器类
  * Note: 运算符重载
@@ -28,15 +29,16 @@ private:
 
 //Note: 内联函数定义在类外，需要额外指明inline
 inline void TriangularIterator::check_integrity() const {
-//    if (_index >= Triangular::_max_elem_cnt) {
-//        //throw iterator_overflow();
-//    }
-//
-//    if (_index >= Triangular::_elems.size()) {
-//        Triangular::
-//    }
-}
+    if (_index >= Triangular::_max_elem_cnt) {
+        //throw iterator_overflow();
+        cerr <<"iterator_overflow" <<endl;
+        return;
+    }
 
+    if (_index >= Triangular::_elems.size()) {
+        Triangular::gen_elems(_index+1);
+    }
+}
 
 inline bool TriangularIterator::operator==(const TriangularIterator &t) const {
     return _index == t._index;
@@ -44,6 +46,22 @@ inline bool TriangularIterator::operator==(const TriangularIterator &t) const {
 inline bool TriangularIterator::operator!=(const TriangularIterator &t) const {
     return  !(*this == t); // Note: 这里其实是调用了重载的==
 }
-
-
+inline int TriangularIterator::operator*() const {
+    check_integrity();
+    return Triangular::_elems[_index];
+}
+inline TriangularIterator& TriangularIterator::operator++() { //Note: ++ 前置版本
+    _index ++;
+    check_integrity();
+    return *this;
+}
+//Note: ++ 后置版本
+//      1. 后置版本，固定写法是用一个int参数，这个参数无实际意义
+//      2. 后置版本，返回的是一个临时变量，非引用，因为后置是先使用再自增
+inline TriangularIterator TriangularIterator::operator++(int) {
+    TriangularIterator it = *this; // 先拷贝一份自身
+    _index ++;
+    check_integrity();
+    return *it;
+}
 #endif //CHAPTER4_TRIANGULARITERATOR_H
